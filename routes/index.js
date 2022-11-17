8231b20cea0adcdc1cbfa6919c3d95c7f715f646
Chris Williamson <chriswill0629@gmail.com>
2022-11-17 14:43:13 -0500

    finished basic message board

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,9 @@ const messages = [
 router.get("/", function (req, res, next) {
     res.render("index", { title: "Mini Messageboard", messages: messages });
 });
+router.post("/new", (req, res, next) => {
+    messages.push({ text: req.body.message, user: req.body.name, added: new Date() });
+    res.redirect("/");
+});
 
 module.exports = router;
